d74101bc8c0bafafff437e254fd96335a5790b3d
Jeffrey Linwood <jlinwood@gmail.com>
2016-10-31 16:46:32 -0500

    SDK Server Starter Kit for Ruby

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'https://rubygems.org'
+gem 'dotenv'
+gem 'faker'
+gem 'sinatra'
+gem 'sinatra-contrib'
+gem 'rack-contrib'
+gem 'twilio-ruby', '~> 5.0.x'
---
+++ app.rb
@@ -0,0 +1,174 @@
+require 'twilio-ruby'
+require 'sinatra'
+require 'sinatra/json'
+require 'dotenv'
+require 'faker'
+require 'rack/contrib'
+
+# Load environment configuration
+Dotenv.load
+
+# Set public folder
+set :public_folder, 'public'
+
+# Parse JSON Body parts into params
+use ::Rack::PostBodyContentTypeParser
+
+# Render home page
+get '/' do
+    redirect '/index.html'
+end
+
+# Render video page
+get '/video' do
+    redirect '/video/index.html'
+end
+
+# Render notify page
+get '/notify' do
+    redirect '/notify/index.html'
+end
+
+# Render IP messaging page
+get '/ipmessaging' do
+    redirect '/ipmessaging/index.html'
+end
+
+# Render sync page
+get '/sync' do
+    redirect '/sync/index.html'
+end
+
+# Basic health check - check environment variables have been configured correctly
+get '/config' do
+    content_type :json
+    {
+        TWILIO_ACCOUNT_SID: ENV['TWILIO_ACCOUNT_SID'],
+        TWILIO_NOTIFICATION_SERVICE_SID: ENV['TWILIO_NOTIFICATION_SERVICE_SID'],
+        TWILIO_APN_CREDENTIAL_SID: ENV['TWILIO_APN_CREDENTIAL_SID'],
+        TWILIO_GCM_CREDENTIAL_SID: ENV['TWILIO_GCM_CREDENTIAL_SID'],
+        TWILIO_API_KEY: ENV['TWILIO_API_KEY']   ,
+        TWILIO_API_SECRET: ENV['TWILIO_API_SECRET'] != '',
+        TWILIO_IPM_SERVICE_SID: ENV['TWILIO_IPM_SERVICE_SID'],
+        TWILIO_SYNC_SERVICE_SID: ENV['TWILIO_SYNC_SERVICE_SID'],
+        TWILIO_CONFIGURATION_SID: ENV['TWILIO_CONFIGURATION_SID']
+    }.to_json
+end
+
+# Generate an Access Token for an application user - it generates a random
+# username for the client requesting a token, and takes a device ID as a query
+# parameter.
+get '/token' do
+  # Create a random username for the client
+  identity = Faker::Internet.user_name
+
+  # Create an access token which we will sign and return to the client
+  token = Twilio::JWT::AccessToken.new ENV['TWILIO_ACCOUNT_SID'],
+  ENV['TWILIO_API_KEY'], ENV['TWILIO_API_SECRET'], 3600, identity
+
+  # Grant the access token Video capabilities (if available)
+  if ENV['TWILIO_CONFIGURATION_SID']
+    grant = Twilio::JWT::AccessToken::ConversationsGrant.new
+    grant.configuration_profile_sid = ENV['TWILIO_CONFIGURATION_SID']
+    token.add_grant grant
+  end
+
+  # Grant the access token IP Messaging capabilities (if available)
+  if params['device'] && ENV['TWILIO_IPM_SERVICE_SID']
+    # Create a unique ID for the client on their current device
+    device = params['device']
+    endpoint_id = "TwilioChatDemo:#{identity}:#{device}"
+
+    # Create the IP Messaging Grant
+    grant = Twilio::JWT::AccessToken::IpMessagingGrant.new
+    grant.service_sid = ENV['TWILIO_IPM_SERVICE_SID']
+    grant.endpoint_id = endpoint_id
+    token.add_grant grant
+  end
+  
+  # Grant the access token Sync capabilities (if available)
+  if params['device'] && ENV['TWILIO_SYNC_SERVICE_SID']
+    # Create a unique ID for the client on their current device
+    device = params['device']
+    endpoint_id = "TwilioSyncDemo:#{identity}:#{device}"
+
+    # Create the Sync Grant
+    grant = Twilio::JWT::AccessToken::SyncGrant.new
+    grant.service_sid = ENV['TWILIO_SYNC_SERVICE_SID']
+    grant.endpoint_id = endpoint_id
+    token.add_grant grant
+  end
+  # Generate the token and send to client
+  json :identity => identity, :token => token.to_jwt
+end
+
+# Notify - create a device binding from a POST HTTP request
+post '/register' do
+
+  # Authenticate with Twilio
+  client = Twilio::REST::Client.new(
+      ENV['TWILIO_API_KEY'],
+      ENV['TWILIO_API_SECRET'],
+      ENV['TWILIO_ACCOUNT_SID']
+  )
+
+  # Reference a valid notification service
+  service = client.notify.v1.services(
+    ENV['TWILIO_NOTIFICATION_SERVICE_SID']
+  )
+
+  begin
+    binding = service.bindings.create(
+      endpoint: params[:endpoint],
+      identity: params[:identity],
+      binding_type: params[:BindingType],
+      address: params[:Address]
+    )
+    response = {
+      message: 'Binding created!',
+    }
+    response.to_json
+  rescue Twilio::REST::TwilioException => e
+    puts e.message
+    status 500
+    response = {
+      message: "Failed to create binding: #{e.message}",
+      error: e.message
+    }
+    response.to_json
+  end
+end
+
+# Notify - send a notification from a POST HTTP request
+post '/send-notification' do
+  # Authenticate with Twilio
+  client = Twilio::REST::Client.new(
+      ENV['TWILIO_API_KEY'],
+      ENV['TWILIO_API_SECRET'],
+      ENV['TWILIO_ACCOUNT_SID']
+  )
+
+  # Reference a valid notification service
+  service = client.notify.v1.services(
+    ENV['TWILIO_NOTIFICATION_SERVICE_SID']
+  )
+
+  begin
+    binding = service.notifications.create(
+      identity: params[:identity],
+      body: 'Hello, ' + params[:identity] + '!'
+    )
+    response = {
+      message: 'Notification Sent!',
+    }
+    response.to_json
+  rescue Twilio::REST::TwilioException => e
+    puts e.message
+    status 500
+    response = {
+      message: "Failed to send notification: #{e.message}",
+      error: e.message
+    }
+    response.to_json
+  end
+end
