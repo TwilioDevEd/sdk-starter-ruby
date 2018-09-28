6334faf1f9cd7139c3a2b0cce910afd6d8149757
Jeffrey Linwood <jlinwood@gmail.com>
2018-09-28 05:51:28 -0500

    Update SDK Versions for Chat and Video

--- public/notify/notify.js
+++ public/notify/notify.js
@@ -1,13 +1,12 @@
 $(function() {
-
-    $('#sendNotificationButton').on('click', function() {
-        $.post('/send-notification', {
-           identity: $('#identityInput').val(),
-           body: "Hello, World!"
-        }, function(response) {
-            $('#identityInput').val('');
-            $('#message').html(response.message);
-            console.log(response);
-        });
+  $('#sendNotificationButton').on('click', function() {
+    $.post('/send-notification', {
+      identity: $('#identityInput').val(),
+      body: "Hello, world!"
+    }, function(response) {
+      $('#identityInput').val('');
+      $('#message').html(response.message);
+      console.log(response);
     });
+  });
 });
